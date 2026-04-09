de0178af249a729d48e7f23419f4a37c02a4463f
ODARI-CHARLES1 <daymondodari68@gmail.com>
2026-04-09 12:56:16 +0300

    feat(wall_follower): implement sensor‑based wall following logic
    Add proximity sensors and motor velocity logic to make the controller
    follow a wall. Sensors are enabled at startup, and the main loop
    checks front and left wall proximity to set motor speeds. This
    introduces new behavior without breaking existing functionality.

--- controllers/wall_follower/wall_follower.py
+++ controllers/wall_follower/wall_follower.py
@@ -1,32 +1,60 @@
 """wall_follower controller."""
 
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
 from controller import Robot
 
-# create the Robot instance.
-robot = Robot()
+def run_robot(robot):
 
-# get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
+    timestep = int(robot.getBasicTimeStep())
 
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  motor = robot.getDevice('motorname')
-#  ds = robot.getDevice('dsname')
-#  ds.enable(timestep)
+    max_speed = 6.28
+    THRESHOLD = 80
 
-# Main loop:
-# - perform simulation steps until Webots is stopping the controller
-while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
+    # Correct motor initialization
+    left_motor = robot.getDevice('left wheel motor')
+    right_motor = robot.getDevice('right wheel motor')
 
-    # Process sensor data here.
+    left_motor.setPosition(float('inf'))
+    right_motor.setPosition(float('inf'))
 
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
-    pass
+    left_motor.setVelocity(0.0)
+    right_motor.setVelocity(0.0)
 
-# Enter here exit cleanup code.
+    # Proximity sensors
+    proxy_sensors = []
+
+    for i in range(8):
+        sensor_name = 'ps' + str(i)
+        sensor = robot.getDevice(sensor_name)
+        sensor.enable(timestep)
+        proxy_sensors.append(sensor)
+
+    while robot.step(timestep) != -1:
+
+        sensor_values = [sensor.getValue() for sensor in proxy_sensors]
+
+        left_wall = sensor_values[5] > THRESHOLD
+        front_wall = sensor_values[7] > THRESHOLD
+
+        left_speed = max_speed
+        right_speed = max_speed
+
+        if front_wall:
+            print("Turn right")
+            left_speed = max_speed
+            right_speed = -max_speed
+
+        elif not left_wall:
+            print("Turn left")
+            left_speed = max_speed * 0.5
+            right_speed = max_speed
+
+        else:
+            print("Follow wall")
+
+        left_motor.setVelocity(left_speed)
+        right_motor.setVelocity(right_speed)
+
+
+if __name__ == "__main__":
+    my_robot = Robot()
+    run_robot(my_robot)
